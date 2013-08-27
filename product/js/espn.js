b540dbdbece845592729a6a361a39e64ccf38dd2
andrewrod33 <andrewrodriguez2012@gmail.com>
2013-08-27 15:11:29 -0700

    After Presentation 1
    Project after my first presentation

--- product/js/espn.js
+++ product/js/espn.js
@@ -1,39 +1,51 @@
 $(document).ready(function(){
+	var teamId= JSON.parse(localStorage.getItem('getTeams'));
+	console.log(teamId);
+	
+		
+	function doAJAX(url){
+		$.ajax(
+			{
+				url: 'http://api.espn.com/v1/sports/football/nfl/teams/'+ url +'/news/?apikey=6pz3x9tcb269689jvg23swm8',	
+				type: 'GET',
+				dataType: 'json',
+				data:{
+				},
+				success: function(json) {
+					console.log(json);
+				$('ul#news').append(Mustache.render($("#new-news").html(), json  )
+					)
+					}
+				}
+		);
+	
 
-	$.ajax(
+	/* $.ajax(
 		{
 			url: 'http://proxy-rocketu.rhcloud.com/rocketu-api/',	
 			type: 'GET',
 			dataType: 'json',
 			data:{
-				url: 'http://api.espn.com/v1/sports/football/nfl/teams/13/news/?apikey=6pz3x9tcb269689jvg23swm8',
+				url: 'http://bleacherreport.com/api/front/lead_articles.json?tags="oakland-raiders"&devicetype=ipad&appversion=1.4&page=1&perpage=10'
 			},
 			success: function(json) {
 				console.log(json);
-			$('ul#news2').html(Mustache.render($("#new-news").html(), json  )
+			$('ul#news2').html(Mustache.render($("#all-news-template").html(), json  )
 				)
 				}
 			}
-	);
+	); */
+	}
+		for ( i=0; i<teamId.length; i++){
+			console.log(i);
+			doAJAX(teamId[i]);
+		}
 
-	$.ajax(
-		{
-			url: 'http://proxy-rocketu.rhcloud.com/rocketu-api/',	
-			type: 'GET',
-			dataType: 'json',
-			data:{
-				url: 'http://bleacherreport.com/api/front/lead_articles.json?tags=oakland-raiders&devicetype=ipad&appversion=1.4&page=1&perpage=10'
-			},
-			success: function(json) {
-				console.log(json);
-			$('ul#news').html(Mustache.render($("#all-news-template").html(), json  )
-				)
-				}
-			}
-	);
+	   
+	 
 
 });
-
+	
 //url: 'http://api.espn.com/v1/sports/football/nfl/teams/13/news/?apikey=6pz3x9tcb269689jvg23swm8',
 //url: 'http://api.espn.com/v1/now/?apikey=6pz3x9tcb269689jvg23swm8',
 //url:'http://api.espn.com/v1/sports/news/headlines/top/?apikey=6pz3x9tcb269689jvg23swm8',
